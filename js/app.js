4dbabedb3c3ffe077435356049da8e12bdbf3675
huy <huy.dvn@digidinos.com>
2021-12-03 17:02:03 +0700

    extension crawl

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const elementNode = document.getElementById('ppd')
+const selector1 = elementNode.querySelectorAll('li.a-spacing-small.item')
+const Images = []
+if (selector1.length) {
+  for (const element of selector1) {
+    Images.push(element.querySelector('img').getAttribute('src'))
+  }
+}
+console.log(Images)
